89ce5e950f6dc661d7c1512e25b0610fd7054c91
Newton Adhikari <newtonadk@gmail.com>
2026-04-21 10:20:47 +0545

    created method to Measure inference latency

---
+++ scripts/eval/measure_inference_latency.py
@@ -0,0 +1,10 @@
+# Measure inference latency for MC-Dropout at various T values.
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+
+
+if __name__ == "__main__":
+    main()
